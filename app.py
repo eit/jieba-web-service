85490a498fcb20f2ca555fd9fd901d90d286f4f7
eit <cheneit@gmail.com>
2016-06-28 20:55:38 +0800

    update app.py for falcon update

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 import falcon
 import seg
 import tag
-
 api = application = falcon.API()
+api.req_options.auto_parse_form_urlencoded = True
 seg = seg.Resource()
 api.add_route('/jieba', seg)
 tag = tag.Resource()
